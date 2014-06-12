74562aef01c00098f84bac39bab86d567bcb5cb3
wusfen <404315887@qq.com>
2014-06-12 14:17:28 +0800

    wui.cookie.js
    wui.cookie.js完整

--- wui.tools/wui.cookie.js
+++ wui.tools/wui.cookie.js
@@ -1,61 +1,56 @@
 /*
 by wushufen
 wusfun@foxmail.com
-2014.06.11
 2014.01.05
+update 2014.06.12
 
-cookie('name');                   //get
-cookie('name', 'value');          //set, add
-cookie('name', null);             //delete
-cookie(null);                     //delete all
-cookie();                         //get all {}
-cookie()['name']                  //get
 cookie('name', 'value', {expires: ms, domain: '', path: '/', secure: true});
+cookie('name', 'value'); //set
+cookie('name', null);    //delete
+cookie(null);            //delete all
+cookie();                //get all {}
+cookie().name            //get
+cookie()['name']         //get
+cookie('name');          //get
 */
-function cookie(name, value, options) {
-	function trim(str){return (''+str).replace(/^\s+|\s+$/g,'')}
+function cookie(name, value, options){
+	options = options||{};
 
 	/*delete: cookie(name, null)*/
-	if (value===null) {
-		options.expires = -1;
-	}
+	if(value===null) options.expires = -1;
 
 	/*delete all: cookie(null)*/
-	if (name===null) {
+	if(name===null){
 		for(i in cookie()){
 			cookie(i,null);
 		}
 	}
 
-	/*set, add: cookie(name, value, {})*/
-	if (value!==undefined) {
-
-		if (options.expires) {
-			var date = new Date();
+	/*set: cookie(name, value, {})*/
+	if(value!==undefined){
+		if(options.expires){
+			var date = new Date;
 			date.setTime(date.getTime() + options.expires);
 		}
-
-		document.cookie=
-			trim(name)+'='+escape(trim(value))//转十六进制
-			+ options.expires? ';expires='+ date.toUTCString():''
-			+ options.path   ? ';path=' + options.path:''
-			+ options.domain ? ';domain=' + options.domain:''
-			+ options.secure ? ';secure':'';
+		document.cookie = [name + '=' + value,
+			options.expires?';expires=' + date.toUTCString() :'',
+			options.path?   ';path=' + options.path :'',
+			options.domain? ';domain=' + options.domain :'',
+			options.secure? ';secure' :''
+		].join('');
 	}
 
 	/*get: cookie('name'), cookie(), cookie().name, cookie()['name']*/
-	var cookies={};
-	var nvs=document.cookie?document.cookie.split(';'):[];//name=value数组
-	for (i in nvs) {
-		var nv = nvs[i].split('=');
-		var n = trim(nv[0]);
-		var v = unescape(nv[1]);//十六进制反转
+	var cookies = {},
+	nvs = document.cookie? document.cookie.split('; ') :[];
+	for(i in nvs){
+		var nv = nvs[i].split('='), n = nv[0], v = nv[1];
 		cookies[n] = v;
 	}
 
 	/*log*/
-	console.log(document.cookie, cookies);
+	// console.log(cookies, document.cookie);
 
-	/*return: value=cookie('name'), obj=cookies()*/
+	/*return: obj=cookies(), value=cookie('name')*/
 	return name===undefined? cookies: cookies[name];
 }
