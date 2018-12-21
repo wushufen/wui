9c1dae730a79c3d96faa1441705d6ee18471cad2
wushufen <wushufen@sunlands.com>
2018-12-21 21:26:37 +0800

    ^ layout container + form input button

--- src/views/index.vue
+++ src/views/index.vue
@@ -5,7 +5,7 @@ html {
 </style>
 
 <style lang="less" scoped>
-.header {
+.layout-header {
   position: relative;
   z-index: 1;
   background: #fff;
@@ -18,35 +18,57 @@ html {
   font-size: 20px;
   text-align: center;
 }
-.sidebar {
-  width: 250px;
-  // background: rgb(8, 32, 36);
-  // background: #060000;
-  background: #fff;
-  // color: #fff;
-}
-.content {
+
+.example {
+  display: block;
   padding: 1em;
+  border-radius: 3px;
+  background: #f5f5f5;
+  color: #555;
+  margin: 1em 0;
 }
-.footer {
-  background: #bbb;
+
+.layout-example {
+  height: 200px;
+  .layout-header {
+    padding: 1em;
+    background: #d0e0ff;
+    border-radius: 5px;
+  }
+  .layout-sidebar {
+    padding: 1em;
+    background: #d0f0ff;
+    border-radius: 5px;
+  }
+  .layout-content {
+    padding: 1em;
+    background: #d0fff4;
+    border-radius: 5px;
+  }
+}
+.container-example {
+  .col {
+    line-height: 3;
+    text-align: center;
+    background: #87d4fa;
+    border-radius: 5px;
+  }
 }
 </style>
 
 <template>
-  <div class="layout">
+  <div class="layout index">
     <div class="layout-row">
-      <div class="sidebar">
-        <div class="header">
+      <div class="layout-sidebar">
+        <div class="layout-header">
           <div class="logo">WUI</div>
         </div>
         <ul class="menu">
           <li>
-            <div> layout </div>
+            <div>layout</div>
           </li>
           <li>
             <div>menu</div>
-            <i class="menu-arrow icon-down"></i>
             <ul>
               <li>
                 <div>sub-menu</div>
@@ -64,75 +86,201 @@ html {
         </ul>
       </div>
       <div class="layout">
-        <div class="header">
+        <div class="layout-header">
           <div class="path mgt">
             <a>home</a>
             <a>page</a>
           </div>
         </div>
-        <div class="layout">
-          <div class="container">
-            <div class="panel">
-              <input type="text">
-              <button>button</button>
-            </div>
-            <div class="panel">
-              <div class="button-group">
-                <button>button</button>
-                <button>button</button>
-                <button>button</button>
-              </div>
-            </div>
-            <div class="panel">
-              <table class="table">
-                <tr>
-                  <th>账号</th>
-                  <th>角色</th>
-                  <th>姓名</th>
-                  <th>性别</th>
-                  <th>手机</th>
-                  <th>操作</th>
-                </tr>
-                <tr>
-                  <td>oooooooo@oo.ooo</td>
-                  <td>oooo</td>
-                  <td>oooooo</td>
-                  <td>oo</td>
-                  <td>ooo-oooo-oooo</td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>oooooooo@oo.ooo</td>
-                  <td>oooo</td>
-                  <td>oooooo</td>
-                  <td>oo</td>
-                  <td>ooo-oooo-oooo</td>
-                  <td></td>
-                </tr>
-              </table>
-              <div class="tac">
-                <div class="pagination">
-                  <span class="icon-left"></span>
-                  <span>1</span>
-                  <span
-                    class="active"
-                    title="1000条/ (10条/页) = 999页"
-                  >5</span>
-                  <span>999</span>
-                  <span class="icon-right"></span>
-                </div>
-              </div>
-            </div>
+        <div class="layout-content">
+          <h3>layout</h3>
+          <div class="panel">
             <div class="row">
-              <div class="col-12">
-                <div class="panel">
-                  panel
+              <div class="col-12 md-24">
+                <div class="example layout-example">
+                  <div class="layout">
+                    <div class="layout-header">header</div>
+                    <div class="layout-content">content</div>
+                  </div>
                 </div>
               </div>
-              <div class="col-12">
-                <div class="panel">
-                  panel
+              <div class="col-12 md-24">
+                <div class="example layout-example">
+                  <div class="layout-row">
+                    <div class="layout-sidebar">sidebar</div>
+                    <div class="layout-content">content</div>
+                  </div>
                 </div>
+              </div>
+              <div class="col-12 md-24">
+                <div class="example layout-example">
+                  <div class="layout">
+                    <div class="layout-header">header</div>
+                    <div class="layout-row">
+                      <div class="layout-sidebar">sidebar</div>
+                      <div class="layout-content">content</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 md-24">
+                <div class="example layout-example">
+                  <div class="layout-row">
+                    <div class="layout-sidebar">sidebar</div>
+                    <div class="layout">
+                      <div class="layout-header">header</div>
+                      <div class="layout-content">content</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-12 md-24 none">
+                <div class="example layout-example">
+                  <div class="layout">
+                    <div class="layout-header">header</div>
+                    <div class="layout-row">
+                      <div class="layout-sidebar">sidebar</div>
+                      <div class="layout-content">content</div>
+                      <div class="layout-sidebar">sidebar</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h3>container</h3>
+          <div class="panel">
+            <div class="example container-example">
+              <div
+                class="container"
+                style="width:100%"
+              >
+                <div class="row">
+                  <div
+                    v-for="i in 24"
+                    :key="i"
+                    class="col xs-8 sm-4 md-2"
+                  >{{i}}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h3>form</h3>
+          <div class="panel">
+            <div class="example">
+              <div class="form">
+                <div class="row">
+                  <label>label</label>
+                  <div>
+                    <input
+                      v-model="input"
+                      placeholder="input"
+                    >
+                  </div>
+                </div>
+                <div class="row">
+                  <label>textarea</label>
+                  <div>
+                    <textarea
+                      xrows="2"
+                      placeholder="textarea"
+                    ></textarea>
+                  </div>
+                </div>
+                <div class="row">
+                  <label>loooooooooong label</label>
+                  <div>
+                    <select>
+                      <option value="1">select</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="row">
+                  <label></label>
+                  <div>
+                    <button>button</button>
+                  </div>
+                </div>
+                <div class="row">
+                  <label>input-group</label>
+                  <div>
+                    <div class="input-group">
+                      <span>span</span>
+                      <select>
+                        <option value="1">select</option>
+                      </select>
+                      <input placeholder="input">
+                      <button>button</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <label>input-group</label>
+                  <div>
+                    <div class="input-group">
+                      <select>
+                        <option value="1">select</option>
+                      </select>
+                      <select>
+                        <option value="1">select</option>
+                      </select>
+                      <select>
+                        <option value="1">select</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <label>button-group</label>
+                  <div>
+                    <div class="button-group">
+                      <button>button</button>
+                      <button>button</button>
+                      <button>button</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <h3>table</h3>
+          <div class="panel">
+            <table class="table">
+              <tr>
+                <th>账号</th>
+                <th>角色</th>
+                <th>姓名</th>
+                <th>性别</th>
+                <th>手机</th>
+                <th>操作</th>
+              </tr>
+              <tr>
+                <td>oooooooo@oo.ooo</td>
+                <td>oooo</td>
+                <td>oooooo</td>
+                <td>oo</td>
+                <td>ooo-oooo-oooo</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>oooooooo@oo.ooo</td>
+                <td>oooo</td>
+                <td>oooooo</td>
+                <td>oo</td>
+                <td>ooo-oooo-oooo</td>
+                <td></td>
+              </tr>
+            </table>
+            <div class="tac">
+              <div class="pagination">
+                <span class="icon-left"></span>
+                <span>1</span>
+                <span
+                  class="active"
+                  title="1000条/ (10条/页) = 999页"
+                >5</span>
+                <span>999</span>
+                <span class="icon-right"></span>
               </div>
             </div>
           </div>
@@ -149,7 +297,7 @@ export default {
   data() {
     vm = this;
     return {
-      data: {}
+      input: ""
     };
   },
   methods: {
@@ -157,6 +305,7 @@ export default {
   },
   mounted() {
     vm.load();
+    window.vue = this;
   }
 };
 </script>
