4ae84ad2bb7093f4322e897770473aefc7be8878
wusfen <wusfun@foxmail.com>
2018-12-24 02:58:50 +0800

    select.js

---
+++ src/styles/select.js
@@ -0,0 +1,12 @@
+var flat = document.createComment('select')
+
+select = $0
+select.addEventListener('mousedown', function () {
+  var parent = select.parentNode
+
+  parent.insertBefore(flat, select)
+  parent.removeChild(select)
+  parent.insertBefore(select, flat)
+  parent.removeChild(flat)
+
+})
