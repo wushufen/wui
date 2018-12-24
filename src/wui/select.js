6a3455743025d50587d866fc454a7f3a12f20846
wushufen <wushufen@sunlands.com>
2018-12-24 18:19:56 +0800

    options pos

--- src/wui/select.js
+++ src/wui/select.js
@@ -2,11 +2,35 @@ import $ from "../libs/$";
 
 var flat = document.createComment('select')
 var options = $(document.createElement('options'))
+options.update = function () {
+  var select = this._select
+
+  options
+    .html(select.innerHTML)
+    .appendTo('body')
+    .show()
+    .resize()
+}
+options.resize = function () {
+  if (!this._select) {
+    return
+  }
+
+  var select = this._select
+  var offset = select.getBoundingClientRect()
+
+  options.css({
+    left: offset.left + 'px',
+    top: offset.top + offset.height + 'px',
+    minWidth: offset.width + 'px'
+  })
+}
 
 $('select').on('mousedown', function () {
   console.log('mousedown')
   var parent = this.parentNode
 
+  // 去掉默认下拉列表
   parent.insertBefore(flat, this)
   parent.removeChild(this)
   parent.insertBefore(this, flat)
@@ -14,19 +38,15 @@ $('select').on('mousedown', function () {
 
 }).on('focus', function () {
   console.log('focus')
-  var self = $(this)
-
-  var offset = this.getBoundingClientRect()
-  options.css({
-    left: offset.left + 'px',
-    top: offset.top + offset.height + 'px',
-    minWidth: offset.width + 'px'
-  })
-    .html(self.html())
-    .appendTo('body').show()
+  options._select = this
+  options.update()
 
 }).on('blur', function () {
   console.log('blur')
   options.hide()
+  options._select = null
+})
 
+$('body').on('scroll', function (e) {
+  options.resize()
 })
